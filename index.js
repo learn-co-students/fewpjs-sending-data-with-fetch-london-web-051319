f785394601ed6d45879fc4c79e15eb0d882bcd10
chrisdews <cldews1@gmail.com>
2019-06-27 16:55:31 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your code here
+
+function submitData(name, email){
+    let formData = {
+        name: name,
+        email: email
+    }
+
+   
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData)
+  };
+   
+   return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      addObjectToDom(object);
+    })
+    .catch(function(error) {
+      addErrorToDom(error);
+    });
+}
+
+function addObjectToDom(object){
+    // debugger
+    const div = document.createElement("div");
+    div.innerHTML = `id ${object.id} name ${object.name}` ;
+    document.body.appendChild(div);
+    
+}
+
+function addErrorToDom(error){
+    // debugger
+    const div = document.createElement("div");
+    div.innerHTML = "Unauthorized Access" ;
+    document.body.appendChild(div);
+    
+}
+submitData('dewsy', 'chris@dews');
