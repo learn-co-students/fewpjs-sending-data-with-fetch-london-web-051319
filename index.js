6c964101e7a9f53f45bb2cc83f95658697fe8229
James <hayward.jp@gmail.com>
2019-06-27 14:01:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+// document.addEventListener('DOMContentLoaded', function(e) {
+//    submitData();
+// });
+
+const body = document.querySelector('body');
+
+let submitData = (name, email) => {
+
+   let formData = {
+      name: name,
+      email: email
+   };
+
+   let configObj = {
+      method: "POST",
+      headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+      },
+      body: JSON.stringify({name: name, email: email})
+   };
+
+   return fetch('http://localhost:3000/users', configObj)
+   .then(function(response) {
+      return response.json();
+   })
+   .then(function(r) {
+      body.innerHTML = r.id;
+   })
+   .catch(function(error) {
+      alert("Gretchen, stop trying to make fetch happen! It's not going to happen!");
+      body.innerHTML = error.message;
+   });
+};
