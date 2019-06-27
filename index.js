57e29a61a8063f72d41267aa11352d166935f4cb
mbarrins <maija.barrins@gmail.com>
2019-06-27 13:08:08 +0100

    added submitData function

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  let formData = {
+    name: userName,
+    email: userEmail
+  };
+   
+  
+  let configObj = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+   
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object.id);
+      addToDOM(object.id);
+    })
+    .catch(function(error) {
+      console.log(error.message);
+      addToDOM(error.message);
+    });
+};
+
+function addToDOM(text) {
+  const body = document.querySelector('body');
+  const p = document.createElement('p');
+  p.innerText = text;
+  body.appendChild(p)
+};
