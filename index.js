44bf8bdbb9e672605acb74cd730ff991279d843c
mbarrins <maija.barrins@gmail.com>
2019-06-27 13:39:48 +0100

    Done.

--- index.js
+++ index.js
@@ -19,18 +19,16 @@ function submitData(userName, userEmail) {
       return response.json();
     })
     .then(function(object) {
-      console.log(object.id);
       addToDOM(object.id);
+      // document.body.append(object.id)
     })
     .catch(function(error) {
-      console.log(error.message);
       addToDOM(error.message);
     });
 };
 
 function addToDOM(text) {
-  const body = document.querySelector('body');
   const p = document.createElement('p');
-  p.innerText = text;
-  body.appendChild(p)
+  p.innerHTML = text;
+  document.body.appendChild(p)
 };
