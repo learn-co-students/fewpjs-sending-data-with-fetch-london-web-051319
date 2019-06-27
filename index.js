4c98e2f002b59a9072b36c7ab289da0a4b086ca2
JackH17 <jack.hawkins77@googlemail.com>
2019-06-27 15:17:48 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const body = document.querySelector('body');
+
+let submitData = (name, email) => {
+
+   let formData = {
+      name: name,
+      email: email
+   };
+
+   let configObj = {
+      method: "POST",
+      headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+      },
+      body: JSON.stringify({name: name, email: email})
+   };
+
+   return fetch('http://localhost:3000/users', configObj)
+   .then(function(response) {
+      return response.json();
+   })
+   .then(function(r) {
+      body.innerHTML = r.id;
+   })
+   .catch(function(error) {
+      alert("Gretchen, stop trying to make fetch happen! It's not going to happen!");
+      body.innerHTML = error.message;
+   });
+};
+   
+   
