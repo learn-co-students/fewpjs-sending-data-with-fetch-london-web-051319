0074649726c8c49cc94d6dfd103ecb8d01e65e90
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-06-27 12:07:37 +0100

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-function submitData( name, email ) {
-    return fetch( 'http://localhost:3000/users', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify( {
-          name,
-          email
-        } )
-      } )
-      
-      .then( function ( response ) {
-        return response.json()
-      } )
-      .then( function ( object ) {
-        document.body.innerHTML = object[ "id" ]
-      } )
-      .catch( function ( error ) {
-        document.body.innerHTML = error.message
-      } )
-  }
+const body = document.querySelector("body")
+
+function submitData (name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify( {name, email} )
+  })
+    .then(response => response.json())
+    .then(object => body.append(object.id))
+    .catch(function (error) {
+      alert('Bad Request')
+      body.append(error.message)
+    })
+}
